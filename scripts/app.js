5e57ac179b4af431e786e01f24bf4c7ff5746116
monkey-d-luffy <ktkopp@icloud.com>
2022-01-24 15:55:05 -0700

    hi

--- scripts/app.js
+++ scripts/app.js
@@ -1,22 +1,62 @@
 /** @type {HTMLCanvasElement} */
-const canvas = document.getElementById('game-canvas');
-const ctx = canvas.getContext('2d');
+const canvas = document.getElementById("game-canvas");
+const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 600;
 
-console.log("Hello world!");
+class Square {
+	constructor(x, y, color) {
+		this.x = x;
+		this.y = y;
+		this.width = 200;
+		this.height = this.width;
+		this.color = color;
+		this.yDirection = 1;
+		this.xDirection = 1;
+	}
 
-let suit = "spades";
-let face = "queen";
-let value = 10;
+	update() {
+		// bounce off the bottom
+		if (this.y + this.height >= canvas.height) {
+			this.yDirection = -1;
+		}
 
-let isFace = true;
+		// bounce off the top
+		if (this.y <= 0) {
+			this.yDirection = 1
+		}
 
-let aceOfSpades = {
-    suit: "spades",
-    face: "ace",
-    value: 1,
-    isFace: false,
-};
+		// bounces off the right
+		if (this.x + this.width >= canvas.width) {
+			this.xDirection = -1;
+		}
 
-console.log(aceOfSpades.tostring());
+		// bounces off the left
+		if (this.x <= 0) {
+			this.xDirection = 1;
+		}
+		this.y += 10 * this.yDirection;
+		this.x += 10 * this.xDirection;
+	}
+
+	draw() {
+		ctx.fillStyle = this.color;
+		ctx.fillRect(this.x, this.y, this.width, this.height);
+	}
+}
+
+let square1 = new Square(100, 200, "red");
+let square2 = new Square(20, 500, "blue");
+
+function animate() {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	square1.update();
+	square2.update();
+
+	square1.draw();
+	square2.draw();
+
+	requestAnimationFrame(animate);
+}
+
+requestAnimationFrame(animate);
---
+++ scripts/guess.js
@@ -0,0 +1,42 @@
+function playGuessingGame() {
+	let secret = Math.floor(Math.random() * 10 + 1);
+	//console.log(secret);
+	let guesses = 0;
+	let guessCount = 3;
+	let playerGuess;
+	let gameOver = false;
+
+	while (guesses < guessCount && !gameOver) {
+		playerGuess = Number(prompt("Pick a number between 1 and 10"));
+
+		if (playerGuess === secret) {
+			// player wins
+			gameOver = true;
+			alert("you get a free win");
+		} else {
+			if (playerGuess < secret) {
+				alert("Too low! you suck");
+			} else {
+				alert("Too high!you suck");
+			}
+		}
+
+		// guesses = guesses + 1;
+		// guesses += 1;
+		guesses++;
+		// ++guesses;
+	}
+
+	if (!gameOver) {
+		alert("You lose!");
+	}
+}
+
+// listen for clicks on the button
+let guessButton = document.getElementById("guess-game");
+// let clickCount = 0;
+// console.log(guessButton);
+
+guessButton.addEventListener("click", () => {
+	playGuessingGame();
+});
